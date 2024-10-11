cfa7f1a2372ea23c0bfe7145837d5b5a946dee17
jerpuen <kittipot.kulpinit@gmail.com>
2024-10-11 20:29:07 +0700

    sent: assignment

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,8 @@
 let height = undefined;
-let result;
+let result = checkHight(height);
+
+function checkHight (height) {
+    return height ?? "Height is not defined";
+}
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
